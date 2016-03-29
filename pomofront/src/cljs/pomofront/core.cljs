72acf321df1d3a4ea6a78d7f15a5d4452db3a6fc
Teemu Sarapisto <tsarpf@gmail.com>
2016-03-29 12:37:39 +0300

    Work on login page

--- pomofront/src/cljs/pomofront/core.cljs
+++ pomofront/src/cljs/pomofront/core.cljs
@@ -14,6 +14,12 @@
     (POST "http://localhost:3000/api/user" 
           {:body object})))
 
+(defn login-handler [email password]
+  (let [object (clj->js {:email email :password password})]
+    (.log js/console object)
+    (POST "http://localhost:3000/api/session" 
+          {:body object})))
+
 
 (defn atom-trim-str [strink]
   (-> @strink str clojure.string/trim))
@@ -26,6 +32,7 @@
    [:div 
     [:div [:a {:href "/about"} "go to about page"]]
     [:div [:a {:href "/register"} "go to register page"]]
+    [:div [:a {:href "/login"} "go to login"]]
     ]])
 
 (defn about-page []
@@ -43,8 +50,23 @@
        [:input {:type "button" :value "Register!" :on-click register}]
        [:div [:a {:href "/"} "go to the home page"]]])))
 
+(defn login-page[]
+  (let [email (reagent/atom "")
+        password (reagent/atom "")
+        login #(login-handler (atom-trim-str email) (atom-trim-str password))]
+    (fn [props]
+      [:div [:h2 "Login page"]
+       [:input {:type "text" :value @email :on-change #(reset! email (-> % .-target .-value))}]
+       [:input {:type "text" :value @password :on-change #(reset! password (-> % .-target .-value))}]
+       [:input {:type "button" :value "Log in!" :on-click login}]
+       [:div [:a {:href "/"} "go to the home page"]]])))
+
+
 (defn current-page []
+  [:div 
+   [:div [(session/get :header)]]
   [:div [(session/get :current-page)]])
+         ]]
 
 ;; -------------------------
 ;; Routes
