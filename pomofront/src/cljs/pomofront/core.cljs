7987fe6cb13245615afd2b5c27ff5d234edf8cf8
Teemu Sarapisto <tsarpf@gmail.com>
2016-03-29 20:47:05 +0300

    Registering and logging in works!

--- pomofront/src/cljs/pomofront/core.cljs
+++ pomofront/src/cljs/pomofront/core.cljs
@@ -13,7 +13,12 @@
 
 (defn login-response-success [response]
   (.log js/console (clj->json response))
-  (session/put! :token (response :token)))
+  (session/put! :token (response :token))
+  (declare user-header)
+  (session/put! :header #'user-header))
+
+(defn register-response-success [response]
+  (.log js/console (clj->json response)))
 
 (defn response-fail [response]
   (.log js/console (clj->json response)))
@@ -23,15 +28,16 @@
     (.log js/console object)
     (POST "http://localhost:3000/api/user" 
           {:body object
+          :headers {:Content-Type "application/json"}
           :error-handler response-fail
-          :format :json 
-          :handler login-response-success})))
+          :handler register-response-success})))
 
 (defn login-handler [email password]
-  (let [object (clj->js {:email email :password password})]
+  (let [object (clj->json {:email email :password password})]
     (.log js/console object)
     (POST "http://localhost:3000/api/session" 
           {:body object 
+          :headers {:Content-Type "application/json"}
           :error-handler response-fail
           :handler login-response-success})))
 
@@ -49,9 +55,12 @@
     [:div [:a {:href "/register"} "Register"]]
     ]])
 
+(defn user-header []
+  [:div [:h6 "Hai user, TODO: show username"]])
+
 (defn about-page []
-  [:div [:h2 "About pomofront"]
-   [:div [:a {:href "/"} "go to the home page"]]])
+  [:div [:h2 "About Clojodoro"]
+   [:div [:a {:href "/"} "Back home"]]])
 
 (defn register-page[]
   (let [email (reagent/atom "")
@@ -62,7 +71,7 @@
        [:input {:type "text" :value @email :on-change #(reset! email (-> % .-target .-value))}]
        [:input {:type "text" :value @password :on-change #(reset! password (-> % .-target .-value))}]
        [:input {:type "button" :value "Register!" :on-click register}]
-       [:div [:a {:href "/"} "go to the home page"]]])))
+       [:div [:a {:href "/"} "Back home"]]])))
 
 (defn login-page[]
   (let [email (reagent/atom "")
