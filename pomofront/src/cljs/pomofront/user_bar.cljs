136eee8a076d8c3f8761597d9eddb3816d01f0fb
Teemu Sarapisto <tsarpf@gmail.com>
2016-04-16 05:47:00 +0300

    Remove unnecessary function

--- pomofront/src/cljs/pomofront/user_bar.cljs
+++ pomofront/src/cljs/pomofront/user_bar.cljs
@@ -2,9 +2,8 @@
   (:require [pomofront.interactions :refer [get-interaction interactions]]))
 
 (defn frp-thing []
-    (fn [props]
       [:div "The count is:" @(get-interaction {:name "clicker" :init-val 0})
        [:div
        [:button {:on-click (interactions "clicker" inc)} "up"]
-       [:button {:on-click (interactions "clicker" dec)} "down"]]]))
+       [:button {:on-click (interactions "clicker" dec)} "down"]]])
 
