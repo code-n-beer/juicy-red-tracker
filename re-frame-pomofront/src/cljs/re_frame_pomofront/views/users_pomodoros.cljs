b420ad1ed9494b2b7669ec1ce1b767a6576555f9
Teemu Sarapisto <tsarpf@gmail.com>
2016-09-18 20:42:45 +0300

    Add minute counts

--- re-frame-pomofront/src/cljs/re_frame_pomofront/views/users_pomodoros.cljs
+++ re-frame-pomofront/src/cljs/re_frame_pomofront/views/users_pomodoros.cljs
@@ -25,35 +25,43 @@
         time (str hours-format ":" minutes-format)]
     time))
 
+(defn count-minutes [pomos]
+  (reduce #(+ %1 (%2 :minutes)) 0 pomos))
+
 (defn task-date-pomodoros [pomodoros]
  [:div
    (for [date pomodoros]
      (let [datestr (first date)
            pomos (second date)
-           num (count pomos)]
+           num (count pomos)
+           num-minutes (count-minutes pomos)]
      [:div {:key datestr}
-      [:h4.day-header [:i datestr]]
+      [:div [:span.day-header [:i datestr]]
+       [:span.day-stats [:strong num] " pomodoros, " num-minutes " minutes."]]
       [:div.pomodoros-for-day {:key datestr}
        [:ul
         (for [pomodoro pomos]
           (let [id (pomodoro :id)
                 date (new js/Date (pomodoro :created_at))
                 time (get-clock date)]
-            [:li {:key id} time]))]
-       [:strong "= " num]]]))])
+            [:li {:key id} time ", for " (pomodoro :minutes) " minutes."]))]]]))])
+
+(defn count-task-minutes [task]
+  )
 
 (defn benis-list [tasks]
   [:div
    (for [task tasks]
      (let [id (task :id)
            task-name (task :name)
+           task-minutes-spent (count-minutes (task :pomodoros))
            pomo-count (count (task :pomodoros))
            date-grouped-pomos (reverse (group-by #(get-date (% :created_at)) (task :pomodoros)))]
        [:div {:key id}
-        [:div.task-header task-name]
+        [:div.task-header-block [:span.task-header task-name] [:span.task-stats " " pomo-count " pomodoros, " task-minutes-spent " minutes"]]
+        (js/console.log (to-json tasks))
         [:div.task
-         [task-date-pomodoros date-grouped-pomos]
-         [:p pomo-count " pomodoros for " [:strong task-name]]]]))])
+         [task-date-pomodoros date-grouped-pomos]]]))])
 
 (defn date-grouped-pomos []
   (let [tasks (re-frame/subscribe [:task-view])]
