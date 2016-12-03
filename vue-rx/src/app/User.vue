88a9f44e914e6361adbdcf53f190b59a53c8f0f7
Teemu Sarapisto <tsarpf@gmail.com>
2016-12-04 00:09:49 +0200

    Logging in works, woohoo

--- vue-rx/src/app/User.vue
+++ vue-rx/src/app/User.vue
@@ -2,6 +2,7 @@
   <div class="login">
     <div v-if="loggedIn" class="logged-in">
       <p> Logged in! </p>
+      <p> {{ loggedIn }} </p>
     </div>
     <div v-else class="not-logged-in">
       <input type="text" placeholder="email" name="email" v-model="email"/>
@@ -12,10 +13,10 @@
 </template>
 
 <script>
-  // The importing has to be done in all components that have Rxjs..
   import Vue from 'vue'
   import Rx from 'rxjs/Rx'
   import VueRx from 'vue-rx'
+// AFAIK Vue.use rx has to be done in all components that want to use Rxjs..
   Vue.use(VueRx, Rx)
 
   import {login} from '../util/auth'
@@ -28,18 +29,12 @@
       }
     },
     subscriptions() {
-      login('herp', 'derp')
-      console.log('vittu nyt hermanni')
       const email = this.$watchAsObservable('email').pluck('newValue')
       const password = this.$watchAsObservable('password').pluck('newValue')
       const creds = email.combineLatest(password)
-            .map(arr => {console.log(arr); return arr})
       const clickedyClack = this.$fromDOMEvent('button[name=login]', 'click')
-            .map(arr => {console.log(arr); return arr})
             .withLatestFrom(creds, (_, [email, pass]) => [email, pass])
-            .map(arr => {console.log(arr); return arr})
             .switchMap(([email, pass]) => login(email, pass))
-            .map(response => console.log(response))
       return {
         loggedIn: clickedyClack
       }
--- vue-rx/src/util/rest.js
+++ vue-rx/src/util/rest.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3000/api'
+const API = 'http://localhost:3002/api'
 function POST(endpoint, data) {
   return fetch(API + endpoint, {
     method: 'post',
