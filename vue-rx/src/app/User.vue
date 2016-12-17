4b1f7a3f17fc9b46f7b91b2eca1abaed9edbaf54
Teemu Sarapisto <tsarpf@gmail.com>
2016-12-17 13:22:44 +0200

    Login/logout about works. except for userdata merge in auth...

--- vue-rx/src/app/User.vue
+++ vue-rx/src/app/User.vue
@@ -2,6 +2,7 @@
   <div class="login">
     <div v-if="userData && !userData.error" class="logged-in">
       <p v-show="userData.token"> Logged in! </p>
+      <button v-show="userData.token" name="logout"> Logout </button>
     </div>
     <p v-if="loginData && loginData.error"> {{ loginData.error }} </p>
     <div v-if="!userData || !userData.token" class="not-logged-in">
@@ -14,7 +15,7 @@
 
 <script>
   import Rx from 'rxjs/Rx'
-  import {login$, userData$} from '../util/auth'
+  import {login, logout, state} from '../util/auth'
   export default {
     name: 'User',
     data() {
@@ -24,24 +25,29 @@
       }
     },
     subscriptions() {
+      const logout$ = this.$fromDOMEvent('button[name=logout]', 'click')
+                           .do(_ => console.log('hoh'))
+                           .do(_ => logout())
+
       const email = this.$watchAsObservable('email').pluck('newValue')
+                           //.do(_ => console.log('mail'))
       const password = this.$watchAsObservable('password').pluck('newValue')
+                           //.do(_ => console.log('pass'))
       const creds = email.combineLatest(password)
+                           //.do(_ => console.log('creds'))
       const loginRes = this.$fromDOMEvent('button[name=login]', 'click')
                            .do(_ => console.log('1'))
                            .withLatestFrom(creds, (_, [email, pass]) => [email, pass])
                            .do(_ => console.log('2'))
-                           .switchMap(([email, password]) => {
-                             console.log('loginninin')
-                             login$.next({email, password})
-                             return [email, password]
-                           })
+                           .switchMap(([email, password]) => login({email, password}))
                            .do(_ => console.log('3'))
+
       //.flatMap(result => {console.log('flatmap'); return result})
       return {
-        loginResponse: loginRes,
-        userData: userData$,
-        loginData: login$.do(_ => console.log('derp'))
+        userData: state.userData$
+                           .do(x => console.log(x)),
+        loginData: loginRes,
+        logout$
       }
     }
   }
--- vue-rx/src/util/auth.js
+++ vue-rx/src/util/auth.js
@@ -7,28 +7,39 @@ const storage = Rx.Observable.fromEvent(window, 'storage')
       .map(_ => ({token: localStorage.getItem('accesstoken')}))
       .startWith({token: localStorage.getItem('accesstoken')})
 
-export const login$ = new Rx.Subject()
-  .do(_ => console.log('one'))
-  .flatMap(creds => rest.POST('/session', creds))
-  .do(_ => console.log('tuu'))
-  .do(res => {
-    if (!res.token) {
-      throw new Error(res.error)
-    }
-    localStorage.setItem('accesstoken', res.token)
-  })
-  .flatMap(_ => rest.GET('/user/dummy'))
-  .do(_ => console.log('trii'))
-  .catch(e => Rx.Observable.of({error: e.message}))
-  .share()
+export const state = {
+  userData$: storage
+    .scan((state, val) => Object.assign(state, val),
+          //start with existing localstorage contents
+          {token: localStorage.getItem('accesstoken')})
+}
 
-export const userData$ = login$.merge(storage)
-  .scan((state, val) => Object.assign(state, val),
-        //start with existing localstorage contents
-        {token: localStorage.getItem('accesstoken')})
-  .share()
+export const login = (creds) => {
+  const login$ = Rx.Observable.fromPromise(rest.POST('/session', creds))
+        .do(_ => console.log('one'))
+        .map(res => {
+          console.log('tuut tuut')
+          if (!res.token) {
+            console.log('not has token')
+            throw new Error(res.error)
+          } else {
+            console.log('haz token')
+          }
+          localStorage.setItem('accesstoken', res.token)
+          return res
+        })
+        .do(_ => console.log('blblbl'))
+        .catch(e => {
+          console.log('errord')
+          console.log(e)
+          return Rx.Observable.of({error: e.message})
+        })
+  state.userData$ = state.userData$.merge(login$)
+  return login$
+}
 
 export const logout = () => {
-  // NYI
+  window.localStorage.removeItem('accesstoken')
+  location.reload()
 }
 
