b3ea1ff4b1769e404b01c52e5670e5801e5d58cb
Teemu Sarapisto <tsarpf@gmail.com>
2016-12-18 00:04:32 +0200

    Add pomodoro list component
    that apparently was missing before and should have been there a long
    time ago...

---
+++ vue-rx/src/app/PomodoroList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>dem pomos</h2>
+    <p> ses {{userData}} sos </p>
+    <ul name="pomodoros">
+      <li v-for="pomo in pomos">
+        {{pomo.length}} {{pomo.name}}
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+  import {state$} from '../util/state'
+  export default {
+    name: 'PomodoroList',
+    subscriptions() {
+      return {
+        userData: state$
+      }
+    }
+    // mabby use a ready made list component?
+    //components: {
+    //  User, Pomodoro
+    //}
+  };
+</script>
