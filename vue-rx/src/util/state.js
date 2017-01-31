270598b0042058ddc841038ff885509ef05935b4
Teemu Sarapisto <tsarpf@gmail.com>
2017-02-01 01:07:28 +0200

    Even cleaner state manager!

--- vue-rx/src/util/state.js
+++ vue-rx/src/util/state.js
@@ -4,24 +4,14 @@ import isEqual from 'lodash.isequal'
 const initialStateObserver = Rx.Observable.of({})
       .map((obj) => state => Object.assign({}, state, obj))
 
-const stateObserver$ = new Rx.BehaviorSubject(initialStateObserver)
-      .scan((stateObservable, newObservable) => Rx.Observable.merge(stateObservable, newObservable))
-      .do(_ => console.log('state observer des'))
-      .do(e => console.log(e))
-
-export const newStateObservable = (observable) => {
-  stateObserver$.next(observable
-                      .share()
-                      .map((obj) => state => Object.assign({}, state, obj)))
-}
-
 // Don't put initial state here because it's not within an observable
 // so the stateObserver$ switchMap below will not listen to it after
 // first other observer comes in
 export const state$ = new Rx.BehaviorSubject({})
 
-const reducer = stateObserver$
-      .do(_ => console.log('one'))
+const reducer = new Rx.BehaviorSubject(initialStateObserver)
+      .scan((stateObservable, newObservable) => Rx.Observable.merge(stateObservable, newObservable))
+      .do(_ => console.log('state observer des'))
       .do(e => console.log(e))
       .switchMap(x => x)
       .scan((state, fn) => fn(state), {})
@@ -30,5 +20,11 @@ const reducer = stateObserver$
       .distinctUntilChanged(isEqual)
       .do(_ => console.log('four'))
       .do(e => console.log(e))
+
+export const newStateObservable = (observable) => {
+  reducer.next(observable
+                      .share()
+                      .map((obj) => state => Object.assign({}, state, obj)))
+}
 
 reducer.subscribe(state$)
