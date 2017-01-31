84af7bbd70cd2d7c8104e639ba0e00e9a0ee6d1b
Teemu Sarapisto <tsarpf@gmail.com>
2017-01-31 20:50:34 +0200

    Cat everywhere. Limit task selection options to ones under selected category

--- vue-rx/src/app/Pomodoro.vue
+++ vue-rx/src/app/Pomodoro.vue
@@ -1,30 +1,59 @@
 <template>
   <div>
-    <input class="pomo-length" v-model="pomodoroLength" placeholder="Pomodoro length in minutes">
-    <select class="task-select">
-      <option v-for="task in state$.tasks" v-bind:value="task.id"> {{task.name}} </option>
+    <div>
+      <input class="new-category" v-model="newCatName" placeholder="New category name">
+      <button name="new-category-button"> Create </button>
+    </div>
+
+    Select category:
+    <select class="category-select" v-model="categorySelect">
+      <option v-for="category in state$.categories" v-bind:value="category.id"> {{category.name}} </option>
     </select>
+
+    <div>
+      <input class="new-task" placeholder="New task name">
+      <button name="new-task-button"> Create </button>
+    </div>
+
+    <input class="pomo-length" v-model="pomodoroLength" placeholder="Pomodoro length in minutes">
+
+    <select class="task-select">
+      <option v-for="task in tasksPerCategory$" v-bind:value="task.id"> {{task.name}} </option>
+    </select>
+
     <button v-show="running" name="stop"> Stop </button>
     <button v-show="running" name="finish"> Finish </button>
     <button v-show="!running"  name="start"> Start </button>
     <p v-show="running"> runnink {{pomodoroTimer}} plop </p>
   </div>
-
 </template>
 
 <script>
   import Rx from 'rxjs/Rx'
+  import groupBy from 'lodash.groupby'
 
   import {state$} from '../util/state'
+  import {POST} from '../util/rest.js'
 
   export default {
     name: 'Pomodoro',
     data() {
       return {
-        pomodoroLength: 25
+        pomodoroLength: 25,
+        categorySelect: false,
+        tasksUnderCategory: []
       }
     },
     subscriptions() {
+      const selectedCategory = this.$watchAsObservable('categorySelect')
+            .pluck('newValue')
+            .do(e => console.log(e))
+
+      const tasksPerCategory$ = state$
+            .filter(s => s.tasks)
+            .map(s => groupBy(s.tasks, t => t.category_id))
+            .combineLatest(selectedCategory, (tasks, selected) => tasks[selected])
+
       const pomodoroLength$ = this.$watchAsObservable('pomodoroLength')
             .pluck('newValue')
             .startWith(25) // TODO: figure out how to not need more than one initialization (25)
@@ -36,6 +65,29 @@
       const clickStart = this.$fromDOMEvent('button[name=start]', 'click')
             .map(e => true)
 
+      const newTaskName = this.$watchAsObservable('newTaskName')
+            .pluck('newValue')
+
+      const clickNewTask = this.$fromDOMEvent("button[name=new-task-button]", 'click')
+            .do(e => console.log('clickedy clack'))
+            .withLatestFrom(newCatName, (click, catName) => catName)
+            .filter(e => e)
+            .do(e => console.log(`taskname: ${e}`))
+      //TODO: task needs category id etc
+            //.map(n => Rx.Observable.fromPromise(POST('/user/task', {'name': n})))
+            .do(e => console.log(e))
+
+      const newCatName = this.$watchAsObservable('newCatName')
+            .pluck('newValue')
+
+      const clickNewCat = this.$fromDOMEvent("button[name=new-category-button]", 'click')
+            .do(e => console.log('clickedy clack'))
+            .withLatestFrom(newCatName, (click, catName) => catName)
+            .filter(e => e)
+            .do(e => console.log(`catname: ${e}`))
+            .map(n => Rx.Observable.fromPromise(POST('/user/category', {'name': n})))
+            .do(e => console.log(e))
+
       const running = Rx.Observable.merge(clickStart, clickStop, clickFinish)
 
       const pomodoroTimer = running.withLatestFrom(pomodoroLength$, (a, b) => [a,b])
@@ -44,7 +96,7 @@
                 ? Rx.Observable.timer(0, 1000).take(length * 60)
                 : Rx.Observable.empty()})
       return {
-        running, pomodoroLength$, pomodoroTimer, state$: state$
+        selectedCategory, tasksPerCategory$, newCatName, clickNewCat, running, pomodoroLength$, pomodoroTimer, state$
       }
     }
   }
