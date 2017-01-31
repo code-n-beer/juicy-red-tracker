9102772074438a1357eea692a98d65a7ab857f74
Teemu Sarapisto <tsarpf@gmail.com>
2017-02-01 01:13:59 +0200

    Clean-up state.js even further

--- vue-rx/src/util/state.js
+++ vue-rx/src/util/state.js
@@ -7,24 +7,15 @@ const initialStateObserver = Rx.Observable.of({})
 // Don't put initial state here because it's not within an observable
 // so the stateObserver$ switchMap below will not listen to it after
 // first other observer comes in
-export const state$ = new Rx.BehaviorSubject({})
 
-const reducer = new Rx.BehaviorSubject(initialStateObserver)
+export const state$ = new Rx.BehaviorSubject(initialStateObserver)
       .scan((stateObservable, newObservable) => Rx.Observable.merge(stateObservable, newObservable))
-      .do(_ => console.log('state observer des'))
-      .do(e => console.log(e))
       .switchMap(x => x)
       .scan((state, fn) => fn(state), {})
-      .do(_ => console.log('three'))
-      .do(e => console.log(e))
       .distinctUntilChanged(isEqual)
-      .do(_ => console.log('four'))
-      .do(e => console.log(e))
+      .startWith({})
 
 export const newStateObservable = (observable) => {
   reducer.next(observable
-                      .share()
                       .map((obj) => state => Object.assign({}, state, obj)))
 }
-
-reducer.subscribe(state$)
