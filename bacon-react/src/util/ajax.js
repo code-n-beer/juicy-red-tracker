285fda0b9ab11c5824bf556401c433c1e158a38c
Teemu Sarapisto <tsarpf@gmail.com>
2017-05-13 15:07:37 +0300

    Add basic ajax funs

---
+++ bacon-react/src/util/ajax.js
@@ -0,0 +1,23 @@
+import Bacon from 'baconjs'
+
+const API = `${location.protocol}//${location.hostname}:3002/api`
+console.log(API)
+
+export const POST = (endpoint, data) =>
+  Bacon.fromPromise(fetch(API + endpoint, {
+    method: 'post',
+    mode: 'cors',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'accesstoken' : localStorage.getItem('accesstoken')
+    })
+  }).then(res => res.json()))
+
+export const GET = (endpoint) =>
+  Bacon.fromPromise(fetch(API + endpoint, {
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'accesstoken' : localStorage.getItem('accesstoken')
+    })
+  }).then(res => res.json()))
