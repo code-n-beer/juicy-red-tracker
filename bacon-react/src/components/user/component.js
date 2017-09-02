7a95f9dde3d63794ef3a4cbb598ba3a7dd9f69dd
Teemu Sarapisto <tsarpf@gmail.com>
2017-09-02 22:43:23 +0300

    Logging in kindasorta works

--- bacon-react/src/components/user/component.js
+++ bacon-react/src/components/user/component.js
@@ -1,60 +1,32 @@
 import React from 'baret'
 import Bacon from 'baconjs'
-//import {register, login} from '../../util/auth.js'
-
-//import {loggedIn$} from '../../util/state.js'
+import {loggedIn$} from '../../util/state.js'
+import {login} from '../../util/auth.js'
 class User extends React.Component {
-  //componentWillMount() {
-  //  GET('/user/')
-  //    .flatMap(res => console.log(res))
-  //}
-  //this will never work   { Bacon.fromBinder(sink => <Login loginInputChanged={sink}/>) }
-                //{Bacon.fromBinder(sink => {
-                //  console.log('ohhi')
-                //  this.onChange = sink})
-                //  .startWith("initial")
-                //  .map(text => console.log(this.onChange) || <Input text={text} onChange={this.onChange}/>)
-                //}
   constructor(props) {
     super(props)
     this.username = new Bacon.Bus()
     this.password = new Bacon.Bus()
+    this.login = new Bacon.Bus()
+    this.register = new Bacon.Bus()
+    this.inputs = this.username.combine(this.password, (email, password) => {return {email: email, password: password}})
+    const login$ = Bacon.when(
+      [this.inputs, this.login], (creds) => login(creds)
+    ).flatMap(o => o)
+    login$.onError(a=>console.log('login failed'))
+    login$.onValue(val => console.log(val) || loggedIn$.push(val))
   }
   render() {
-    console.log('wat')
-
-    const inputs = this.username.combine(this.password, (user, pass) =>
-    [user,pass])
-    .map(([user, pass]) => {
-      <div>
-        <Input text={user} text$={this.username}/>
-        <Input text={pass} text$={this.password}/>
-      </div>
-    })
-    console.log('adlkjg;dlkdlkjd;hlfj')
     return <div>
-                <p> User Bar </p>
-                <button> Login </button>
-                <button> Register </button>
-                {inputs
-    .startWith(['vittu','saatana'])
-                }
+              <p> User Bar </p>
+              <button onClick={() => this.login.push('clicked')}> Login </button>
+              <button> Register </button>
+              <Input text$={this.username}/>
+              <Input text$={this.password}/>
            </div>
   }
 }
 
-//class Login extends React.Component {
-//  render = () =>
-//    <div>
-//      <div> Login:
-//        <Input/>
-//      </div>
-//    </div>
-//}
+const Input = ({text, text$}) => <input type="text" onChange={(e) => text$.push(e.target.value)}/>
 
-const Input = ({text, text$}) => <input type="text" value={text} onChange={(e) => text$.push(e.target.value)}/>
-
-//class Input extends React.Component {
-//  render = ({text, text$}) => <input type="text" value={text} onChange={(e) => text$.push(e.target.value)}/>
-//}
 export default User
--- bacon-react/src/util/auth.js
+++ bacon-react/src/util/auth.js
@@ -1,9 +1,10 @@
-import {POST} from '../../util/ajax.js'
+import Bacon from 'baconjs'
+import {POST} from './ajax.js'
 const storage$ = Bacon.fromEvent(window, 'click')
       .onValue(() => ({token: localStorage.getItem('accesstoken') }))
 
 export const register = (creds) => POST('/user', creds)
-export const login = (creds) => POST('/session', creds)
+export const login = (creds) => console.log('login') || POST('/session', creds)
 
 export const logout = () => {
   window.localStorage.removeItem('accesstoken')
